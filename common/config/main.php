c4d7e6f18dae3d327d4a72c1bbadb8b6daf0f898
Prabowo Murti <prabowo.murti@gmail.com>
2016-05-07 03:36:05 +0700

    Beautify URL

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,14 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl'     => true,
+            'showScriptName'      => false,
+            'enableStrictParsing' => false,
+            'rules' => [
+                //...
+            ]
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
